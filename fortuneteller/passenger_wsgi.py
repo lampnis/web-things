1452f23c70c96fce5ca98b8255030b28a2a41386
adams <adams@lapins.lv>
2025-04-29 13:28:49 +0300

    removed passenger file

--- fortuneteller/passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from fortuneteller.wsgi import application
-
-sys.path.insert(0, os.path.dirname(__file__))
-wsgi = imp.load_source('wsgi', 'passenger_wsgi.py')
-application = wsgi.application
