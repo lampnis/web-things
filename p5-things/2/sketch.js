8b63259ccc5267d7b71d0fc5e78c06d244438937
adams <adams@lapins.lv>
2025-05-05 23:44:03 +0300

    fixed the colors

--- p5-things/2/sketch.js
+++ p5-things/2/sketch.js
@@ -1,17 +1,17 @@
 function colwheel(n) {
   if (n == 0) {
-    return 'rgb(0, 0, 0)';
+    return color(0, 0, 0);
   }
   if (n > 0 && n <= 85) {
-    return `rgb(0, ${int(255 - n * 3)}, ${int(3 * n)})`;
+    return color(0, int(255 - n * 3), int(3 * n));
   }
   if (n > 85 && n <= 170) {
     n -= 85
-    return `rgb(${int(n)} * 3), 0, ${int(255 - n * 3)})`;
+    return color(int(n * 3), 0, int(255 - n * 3));
   }
   if (n > 170 && n <= 255) {
     n -= 170
-    return `rgb(${int(255 - n * 3)}, ${int(n * 3)}, 0)`;
+    return color(int(255 - n * 3), int(n * 3), 0);
   }
 }
 
@@ -26,26 +26,18 @@ function setup() {
 
 
 let colwheel1 = Math.floor(255 * Math.random());
-let colwheel2 = Math.floor(colwheel1 / 2);
-let updateAfterFrames = 60;
-let startFrames = 0;
+// let colwheel2 = Math.floor(colwheel1 / 2);
 
 function draw() {
-  if (colwheel2 == 255) {
-    colwheel2 = 0;
-  } else {
-    colwheel2 += 1;
+  if (frameCount % 5 == 0) {
+    colwheel1 = (colwheel1 + 1) % 256;
+    // colwheel2 = (colwheel2 + 1) % 256;
   }
 
-  if (colwheel1 == 255) {
-    colwheel1 = 0;
-  } else {
-    colwheel1 += 1;
-  }
-
-  background(colwheel2);
+  circleSize = 1000 / Math.sqrt((Math.abs(mouseX - windowWidth / 2) + Math.abs(mouseY - windowHeight / 2)));
+  // background(colwheel2);
   fill(colwheel1);
-  circle(mouseX, mouseY, 1000 / Math.sqrt((Math.abs(mouseX - windowWidth / 2) + Math.abs(mouseY - windowHeight / 2))));
+  circle(mouseX, mouseY, circleSize);
 
 }
 
