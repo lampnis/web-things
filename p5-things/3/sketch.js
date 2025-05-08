f33f8a15c9b931ca708e512da6b3829f2699e9e0
adams <adams@lapins.lv>
2025-05-08 22:58:21 +0300

    copy from other pc

--- p5-things/3/sketch.js
+++ p5-things/3/sketch.js
@@ -4,30 +4,34 @@ function setup() {
 }
 
 function draw() {
-  var r = map(sin(frameCount / 2), -1, 1, 100, 200);
+
+  var r = map(sin(frameCount / 2), -1, 1, 100, 200)
   var g = map(i, 0, 50, 100, 200);
   var b = map(cos(frameCount), -1, 1, 100, 200);
-
+  console.log("mouseX: ", mouseX)
+  console.log("mouseY: ", mouseY)
   background(g, b, r);
   rotateX(60);
   var mouse_scale_x = map(mouseX, 0, windowWidth, 0.0, 5.0) - 2.5;
-  var mouse_scale_y = map(mouseY, 0, windowHeight, 0.0, 2.0) - 1.0;
+  var mouse_scale_y = map(mouseY, 0, windowHeight, 0.0, 2.0) - 1.0; 
   noFill();
   stroke(r, g, b);
 
-  for (var i; i < 70; i++) {
+  for (var i = 0; i < 70; i++) {
     beginShape();
-    rotate(3 * mouse_scale_x);
+    rotate(3*mouse_scale_x);
     for (var j = 0; j < 360; j += 60) {
       var radius = i * 4.5;
       var x = radius * cos(j);
       var y = radius * sin(j);
-      var z = mouse_scale_y * 100 * sin(frameCount * 2 + i * 10);
-
+      var z = mouse_scale_y * 100*sin(frameCount*2 + i*10);
+      
       vertex(x, y, z);
     }
     endShape(CLOSE);
+  
   }
+
 }
 
 function windowResized() {
