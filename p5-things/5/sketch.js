80ec9656defd59966f4a93070b94827e072eb095
adams <adams@lapins.lv>
2025-07-16 23:12:16 +0300

    added random walker p5 thing

---
+++ p5-things/5/sketch.js
@@ -0,0 +1,42 @@
+class Walker {
+  constructor() {
+    this.x = width / 2;
+    this.y = width / 2;
+  }
+
+  show() {
+    stroke(255);
+    point(this.x, this.y);
+  }
+
+  step() {
+    let choice = floor(random(4));
+    if (choice === 0) {
+      this.x++;
+    } else if (choice === 1) {
+      this.x--;
+    } else if (choice === 2) {
+      this.y++;
+    } else {
+      this.y--;
+    }
+  }
+}
+
+let walker;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  walker = new Walker();
+  background(0);
+}
+
+function draw() {
+  walker.step();
+  walker.show();
+}
+
+// Optional: Adjust canvas size when window is resized
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
