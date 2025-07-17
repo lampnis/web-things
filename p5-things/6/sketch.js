0fd33d501b8c0543e4629d65475b75464d5009a4
adams <adams@lapins.lv>
2025-07-17 16:46:29 +0300

    multiple lines...?

--- p5-things/6/sketch.js
+++ p5-things/6/sketch.js
@@ -1,4 +1,5 @@
 let t = 0.0;
+const numLines = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -7,17 +8,21 @@ function setup() {
 
 function draw() {
   background(0);
-  let xoff = t;
-  noFill();
-  stroke(255);
-  strokeWeight(2);
-  beginShape();
-  for (let i = 0; i < width; i++) {
-    let y = noise(xoff) * height;
-    xoff += 0.01;
-    vertex(i, y);
+
+
+  for (let i = 0; i < numLines; i++) {
+    let xoff = t + i * 1e4;
+    noFill();
+    stroke(10, 25 * i, 10);
+    strokeWeight(2);
+    beginShape();
+    for (let i = 0; i < width; i++) {
+      let y = noise(xoff) * height * (i / 5);
+      xoff += 0.01 * i;
+      vertex(i, y);
+    }
+    endShape();
   }
-  endShape();
   t += 0.01;
 }
 
