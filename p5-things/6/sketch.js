f0e9501c50f7a9b702c50df1a302887e954d1bd9
adams <adams@lapins.lv>
2025-07-28 16:10:13 +0300

    added two sliders

--- p5-things/6/sketch.js
+++ p5-things/6/sketch.js
@@ -1,8 +1,17 @@
+let sliderOne;
+let sliderTwo;
 let t = 0.0;
 const numLines = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  sliderOne = createSlider(0, 20, 9, 0);
+  sliderTwo = createSlider(0, 10, 2, 0);
+  sliderOne.position(10, 10)
+  sliderTwo.position(10, 30)
+  sliderOne.size(150)
+  sliderTwo.size(150)
+
 }
 
 
@@ -13,11 +22,11 @@ function draw() {
   for (let j = 0; j < numLines; j++) {
     let xoff = t + j * 1e4;
     noFill();
-    stroke(map(sin(frameCount / 1e1 * j), 0, 1, 0, 255), 25 * j, 10 * j);
+    stroke(map(sin(frameCount / Math.pow(10, sliderOne.value()) * j), 0, 1, 0, 255), 25 * j, 10 * j);
     strokeWeight(2);
     beginShape();
     for (let i = 0; i < width; i++) {
-      let y = map(noise(xoff), 0, 1, 0, height) * j / map(sin(frameCount / 1e2 * i), 0, 1, -100, 100);
+      let y = map(noise(xoff), 0, 1, 0, height) * j / map(sin(frameCount * (i / Math.pow(10, sliderTwo.value()))), 0, 1, -100, 100);
       xoff += 0.01;
       vertex(i, y);
     }
