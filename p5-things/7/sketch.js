bbe6242f59e2e3624e4956d7731401fb55569932
adams <adams@lapins.lv>
2025-08-24 21:52:47 +0300

    updated the 'new'

--- p5-things/7/sketch.js
+++ p5-things/7/sketch.js
@@ -49,23 +49,24 @@ function setup() {
 
 function draw() {
   background(0);
+  walker.step();
+  walker.show();
 
-
-  for (let j = 0; j < numLines; j++) {
-    let xoff = t + j * 1e4;
-    noFill();
-    stroke(map(sin(frameCount / Math.pow(10, sliderOne.value()) * j), 0, 1, 0, 255), 25 * j, 10 * j);
-    strokeWeight(2);
-    beginShape();
-    for (let i = 0; i < width; i++) {
-      let y = map(noise(xoff), 0, 1, 0, height) * j / map(sin(frameCount * (i / Math.pow(10, sliderTwo.value()))), 0, 1, -100, 100);
-      xoff += 0.01;
-      vertex(i, y);
-    }
-    endShape();
-  }
-
-  t += 0.01;
+  // for (let j = 0; j < numLines; j++) {
+  //   let xoff = t + j * 1e4;
+  //   noFill();
+  //   stroke(map(sin(frameCount / Math.pow(10, sliderOne.value()) * j), 0, 1, 0, 255), 25 * j, 10 * j);
+  //   strokeWeight(2);
+  //   beginShape();
+  //   for (let i = 0; i < width; i++) {
+  //     let y = map(noise(xoff), 0, 1, 0, height) * j / map(sin(frameCount * (i / Math.pow(10, sliderTwo.value()))), 0, 1, -100, 100);
+  //     xoff += 0.01;
+  //     vertex(i, y);
+  //   }
+  //   endShape();
+  // }
+  //
+  // t += 0.01;
 }
 
 // Optional: Adjust canvas size when window is resized
