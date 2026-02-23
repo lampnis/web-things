0bc951c97cf989c1f89fced8cb5764fcb437b54d
Andrew <andrewholland392@gmail.com>
2026-02-23 04:10:01 +0200

    latest changes, added next/prev blogging system

---
+++ blogs.js
@@ -0,0 +1,50 @@
+var maxId;
+var currentId;
+const displayElement = document.getElementById('blog-content');
+const prevBtn = document.getElementById('prev-button')
+const nextBtn = document.getElementById('next-button')
+
+fetch('./blogs.conf')
+  .then(response => response.text())
+  .then(text => {
+    maxId = parseInt(text.trim());
+    currentId = maxId;
+    console.log("Latest post number is:", maxId);
+
+    loadNote(maxId);
+  });
+
+function loadNote(id) {
+  const filename = './blogs/' + id + '.md';
+
+  fetch(filename)
+    .then(response => response.text())
+    .then(blogText => {
+      displayElement.innerText = blogText;
+    })
+    .catch(error => {
+      displayElement.innerText = "Error loading post.";
+      console.error(error);
+    });
+}
+
+function getNext() {
+  if (currentId >= maxId) {
+    console.log("Already at the newest note!")
+  } else {
+    currentId += 1;
+    loadNote(currentId);
+  }
+}
+
+function getPrevious() {
+  if (currentId <= 1) {
+    console.log("Already at the oldest note!")
+  } else {
+    currentId -= 1;
+    loadNote(currentId);
+  }
+}
+
+prevBtn.addEventListener('click', getPrevious)
+nextBtn.addEventListener('click', getNext)
