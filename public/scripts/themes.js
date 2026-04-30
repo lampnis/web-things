10eb190e4ab7f5c303248ca614f16a73e60e5aa0
andyoffice <adams@lapins.lv>
2026-04-30 15:02:24 +0300

    added wallpaper....and some kind of theme consistency among two pages...

--- public/scripts/themes.js
+++ public/scripts/themes.js
@@ -1,5 +1,20 @@
 const themeSwitcher = document.querySelector('.theme-switcher');
 const buttons = themeSwitcher.querySelectorAll('button');
+const wallpapers = {
+    'dark': 'theme-wallpapers/dark.png',
+    'light': 'theme-wallpapers/light.png',
+    'blood-orange': 'theme-wallpapers/bloodorange.png',
+    'pink-moon': 'theme-wallpapers/pinkmoon.png'
+}
+
+const updateWallpaper = (theme) => {
+    const wallpaperElements = document.querySelectorAll('.wallpaper img');
+    wallpaperElements.forEach((img) => {
+        if (wallpapers[theme]) {
+            img.src = wallpapers[theme];
+        }
+    });
+}
 
 const handleThemeSelection = (event) => {
   const target = event.target;
@@ -10,12 +25,14 @@ const handleThemeSelection = (event) => {
   const previouslyPressedButton = document.querySelector('[data-theme][aria-pressed="true"]');
   previouslyPressedButton.setAttribute('aria-pressed', false);
   target.setAttribute('aria-pressed', 'true');
-  
+  updateWallpaper(theme);
   localStorage.setItem('selected-theme', theme);
 }
 
 const savedTheme = localStorage.getItem('selected-theme');
 const defaultTheme = "dark";
+const toApply = savedTheme || defaultTheme;
+updateWallpaper(toApply);
 
 if (savedTheme && savedTheme !== defaultTheme) {
   const previouslyPressedButton = document.querySelector('[data-theme][aria-pressed="true"]');
