9a397bc29cf9626da4e0d3c59909840c8a219e77
andyoffice <adams@lapins.lv>
2026-05-15 18:47:38 +0300

    the new sketch

--- public/p5-things/9/sketch.js
+++ public/p5-things/9/sketch.js
@@ -4,8 +4,9 @@ let boxSide;
 let ballRadius;
 let r;
 let theta;
-let sliderOne;
-let sliderTwo;
+let sliderLine;
+let sliderCircle;
+
 
 function setup() {
     createCanvas(windowWidth, windowHeight, WEBGL);
@@ -20,18 +21,21 @@ function draw() {
     background(0);
     // translate(width/2, height/2);
 
-    let x = r * cos(theta);
-    let y = r * sin(theta);
+    // let x = r * cos(theta);
+    // let y = r * sin(theta);
+    let position = p5.Vector.fromAngle(theta); // would be the same as above two lines
+    position.mult(r);
+
 
     fill(127);
     stroke(255);
-    line(0, 0, x, y);
-    circle(x, y, 48);
+    line(0, 0, position.x, position.y);
+    circle(position.x, position.y, 48);
     theta += 0.0125;
 
 }
 
 // Optional: Adjust canvas size when window is resized
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizecanvas(windowwidth, windowheight);
 }
